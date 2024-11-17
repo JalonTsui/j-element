0dbff8f26fbde0b2ed1c68021a34caec5fb3cb1a
jalontsui <443415003@qq.com>
2024-11-17 15:42:34 +0800

    init vitest

---
+++ src/components/Placeholder/calculator.test.tsx
@@ -0,0 +1,8 @@
+// import {expect,describe,test} from "vitest";
+
+// calls the original implementation,
+// but allows asserting the behaviour later
+describe('test plus',()=>{
+  test('a',()=>{
+  });
+});
