f9fe4217a4d82fd840c65d68d553920424f40294
jalontsui <443415003@qq.com>
2024-11-24 04:03:39 +0800

    vitest init

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
 import viteConfig from './vite.config';
-import { resolve } from 'path';
 
 export default mergeConfig(viteConfig, defineConfig({
   test: {
@@ -8,5 +7,6 @@ export default mergeConfig(viteConfig, defineConfig({
     // alias:{
     //   "$test":resolve(__dirname,"./mocks/$test.ts")
     // }
+    environment:"jsdom"
   },
 }));
